275979ad164dc68081ec961d77d0c06be013eb69
lpaz192 <lpaz@uade.edu.ar>
2024-08-23 15:13:53 -0300

    Agregue el archivo Desafio3.py

---
+++ Desafio3.py
@@ -0,0 +1,12 @@
+
+
+
+
+menu=0
+while menu!=0:
+    print("*************Menú******************")
+    print("1. para ")
+    print("2. para")
+    print("3. para")
+    menu=int(input("Ingrese -1 para finalizar el programa")) 
+    print() 
